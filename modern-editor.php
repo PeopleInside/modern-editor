92e6ab111a44e03adfef1a47399b15211ce61525
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-06-28 12:22:34 +0200

    Update modern-editor.php

--- modern-editor.php
+++ modern-editor.php
@@ -5,6 +5,16 @@ use Grav\Common\Plugin;
 use Grav\Common\Grav;
 use RocketTheme\Toolbox\Event\Event;
 
+/*
+ * Modern Editor Plugin for Grav
+ * Copyright (C) [Anno] [Tuo Nome/PeopleInside]
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
 /**
  * Modern Editor Plugin for Grav 2.0
  *
